50cf50e160463ba75c17284e0763037b054646aa
Linnnn <speak14152@gmail.com>
2024-09-09 23:01:14 +0800

    update: home page & login page (#8)

--- frontend/src/components/BaseFooter.vue
+++ frontend/src/components/BaseFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-indigo-600 text-white text-center py-4 mt-8">
+  <footer class="bg-indigo-600 text-white text-center py-4">
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-white">
       <p>&copy; {{ currentYear }} ExpoVerse. 保留所有權利。</p>
     </div>
--- frontend/src/components/CategoriesSection.vue
+++ frontend/src/components/CategoriesSection.vue
@@ -1,10 +1,11 @@
 <template>
   <section class="mb-16">
-    <h2 class="text-2xl font-semibold mb-6">熱門展館</h2>
+    <h2 class="text-2xl font-semibold mb-6">{{ sectionTitle }}</h2>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
       <BaseCard v-for="(category, index) in categories" :key="index" class="hover:shadow-lg transition-shadow">
         <template #header>
-          <CardTitle class="text-lg font-semibold">{{ category }}</CardTitle>
+          <!-- <CardTitle class="text-lg font-semibold">{{ category }}</CardTitle> -->
+          <h3 class="text-lg font-semibold">{{ category }}</h3>
         </template>
         <template #content>
           <router-link to="/vendor" href="#" class="text-blue-600 flex items-center hover:underline">
@@ -19,18 +20,23 @@
   <script>
   import { ChevronRight } from 'lucide-vue-next'
   import BaseCard from './ui/BaseCard.vue'
-  import CardTitle from './ui/BaseCard.vue'
+  // import CardTitle from './ui/BaseCard.vue'
   
   export default {
     name: 'CategoriesSection',
     components: {
       BaseCard,
-      CardTitle,
+      // CardTitle,
       ChevronRight
     },
-    data() {
-      return {
-        categories: ['餐飲', '零售', '教育', '美容健康']
+    props: {
+      sectionTitle: {
+        type: String,
+        default: '熱門品牌展館'
+      },
+      categories: {
+        type: Array,
+        default: () => ['餐飲', '零售', '教育', '美容健康']
       }
     }
   }
