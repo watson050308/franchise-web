5f758ff2328b05b21fee9abad91705adc83b9c34
xin tian <44495628+watson050308@users.noreply.github.com>
2024-10-23 23:53:52 +0800

    modify homePage CategoriesSection (#17)

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -3,14 +3,14 @@
   <BaseSlide />
   <div class="flex-grow max-w-6xl w-full mx-auto px-4 sm:px-6 lg:px-8 py-16">
     <CategoriesSection
-      sectionTitle="熱門加盟展館" 
-      :categories="franchiseCategories"
+      sectionTitle="熱門展館" 
+      :categories="expoCategories"
       class="mb-16"
     />
-    <CategoriesSection
+    <!--<CategoriesSection
       sectionTitle="熱門寵物展館" 
       :categories="petCategories"
-    />
+    />-->
   </div>
 </template>
   
@@ -27,7 +27,14 @@
   },
   data() {
     return {
-      franchiseCategories: ['紅橘子', '紅吉子', '紅橙子', '紅葡子'],
+      expoCategories: [
+        {category: '加盟展', label: 'franchiseExpo'},
+        {category: '寵物展', label: 'petExpo'},
+        {category: '旅遊展', label: 'travelExpo'},
+        {category: '科技展', label: 'techExpo'},
+        {category: '藝術展', label: 'artExpo'},
+        {category: '美食展', label: 'foodExpo'}
+      ],
       petCategories: ['寵物食品', '寵物用品', '寵物服務', '寵物醫療']
     }
   }
