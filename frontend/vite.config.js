cfbc0a99ec486ef9b0db581aff2c06e9d0d1207d
YochLin <speak14152@gmail.com>
2024-10-26 10:41:10 +0800

    update: package & vue version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/expoverse',
+  server: {
+    port: 5173,
+    // open: true // auto open the web
+  }
 })
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/expoverse/',
+  publicPath: '/franchise-web/', 
   transpileDependencies: true,
 })
