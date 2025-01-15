6d9edd7911f8509773cb7b451dfa142a01d30b81
xin tian <44495628+watson050308@users.noreply.github.com>
2025-01-15 23:10:53 +0800

    Add Franchise Pages (#30)

--- frontend/src/components/ManageMainContent.vue
+++ frontend/src/components/ManageMainContent.vue
@@ -26,6 +26,10 @@ import { Eye } from 'lucide-vue-next';
 import VendorInfoForm from './ui/VendorInfoForm.vue';
 import PreviewModal from './ui/PreviewModal.vue';
 import ManageFranchiseAbout from './ui/franchise/ManageFranchiseAbout.vue';
+import ManageFranchiseProduct from './ui/franchise/ManageFranchiseProduct.vue';
+import ManageFranchiseAdvantages from './ui/franchise/ManageFranchiseAdvantages.vue';
+import ManageFranchiseProcess from './ui/franchise/ManageFranchiseProcess.vue';
+import ManageFranchisePromotions from './ui/franchise/ManageFranchisePromotions.vue';
 
 export default {
   name: 'ManageMainContent',
@@ -42,7 +46,14 @@ export default {
       switch (props.activeTab) {
         case '關於品牌':
           return ManageFranchiseAbout;
-
+        case '產品服務':
+          return ManageFranchiseProduct;
+        case '加盟優勢':
+          return ManageFranchiseAdvantages;
+        case '加盟流程':
+          return ManageFranchiseProcess;
+        case '優惠活動':
+          return ManageFranchisePromotions;
         case '廠商資訊':
           return VendorInfoForm;
         default:
